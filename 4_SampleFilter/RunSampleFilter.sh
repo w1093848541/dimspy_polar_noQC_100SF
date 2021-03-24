6b94cc77fcb822927d81a948bb5046589a9d2502
w1093848541 <77172096+w1093848541@users.noreply.github.com>
2021-03-24 18:16:32 -0400

    Update RunSampleFilter.sh

--- 4_SampleFilter/RunSampleFilter.sh
+++ 4_SampleFilter/RunSampleFilter.sh
@@ -19,7 +19,7 @@ echo "Starting Sample Filter"
 dimspy sample-filter \
 --input ../3_BlankFilter/blankFilter.* \
 --output sampleFilter.$PBS_JOB \
---min-fraction 0.75
+--min-fraction 0.85
 
 #options to consider: --within, -rsd_threshold
 
